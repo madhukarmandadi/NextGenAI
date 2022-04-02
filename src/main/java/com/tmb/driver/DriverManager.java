8475aec8d01a757dc70bfd5a5e9997ab56c247da
Madhukar <madhukarmandadi.qa@gmail.com>
2022-04-02 15:35:48 +0530

    adding new files java

--- src/main/java/com/tmb/driver/DriverManager.java
+++ src/main/java/com/tmb/driver/DriverManager.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
 
-	public static ThreadLocal<WebDriver> dr= new ThreadLocal<WebDriver>();
+	public static ThreadLocal<WebDriver> dr= new ThreadLocal<>();
 
 	public static void setDriver(WebDriver driverref) {
+		System.out.println(dr);
 		dr.set(driverref);
+		System.out.println(dr);
 	}
 
 	public static WebDriver getDriver() {
